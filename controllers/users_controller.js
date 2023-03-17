df345cf77fdd49fe2c4a79550429dd5ee4ff06f6
Ajeya <ajeyasengar@gmail.com>
2023-03-17 22:37:04 +0530

    fourteenth commit: adding sign-in, sign-out and manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,22 @@
 const User = require('../models/user');
 
-module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'Profile Page'
-    });
+module.exports.profile = async function(req, res){
+    if(req.cookies.user_id){
+        let user;
+        try {
+            user = await User.findById(req.cookies.user_id);
+        } catch(err) {
+
+        }
+        if(user){
+            return res.render('user_profile', {
+                title: 'Profile Page',
+                user: user
+            });
+        }
+    } else {
+        return res.redirect('/users/sign-in');
+    }
 };
 
 module.exports.signUp = function(req, res){
@@ -44,6 +57,28 @@ module.exports.create = async function(req, res){
     }
 };
 
-module.exports.createSession = function(req, res){
-
+module.exports.createSession = async function(req, res){
+    let user;
+    try{
+        user = await User.findOne({email: req.body.email});
+    } catch(err){
+        console.log(err);
+        return res.redirect('back');
+    }
+    if(user){
+        if(user.password != req.body.password){
+            console.log('Password did not match, please try again');
+            return res.redirect('back');
+        }
+        res.cookie('user_id', user.id);
+        return res.redirect('/users/profile');
+    } else {
+        console.log('User not found, please try again');
+        return res.redirect('back');
+    }
 };
+
+module.exports.signOut = function(req, res){
+    res.cookie('user_id', '');
+    return res.redirect('back');
+}
